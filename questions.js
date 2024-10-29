877dc478851c56ae0217f6287a84a1d7025e9690
Raika <pejuvenelle@hotmail.com>
2024-10-29 15:26:55 +0100

    rajout des questions et réponses

---
+++ questions.js
@@ -0,0 +1,32 @@
+export const quiz_cinema = {
+    questions: [
+      {
+        text: "Quel est le plus gros succès au box office de James Cameron ?",
+        options: ["Avatar", "Titanic","Terminator 2"],
+        correct_answer: "Avatar"
+      },
+      {
+        text: "Quelle est l'actrice la plus oscarisée au monde ?",
+        options: ["Jane Fonda","Katherine Hepburn","Mery Streep"],
+        correct_answer: "Katherine Hepburn"
+      },
+      {
+        text:"Parmi ces 3 films, lequel a eu une projection 3D en premier au cinéma ?",
+        options:["Pacific Rim (2013)","Bwana Devil (1952)","Green Lantern (2011)"],
+        correct_answer: "Bwana Devil (1952)"
+      },
+
+      {
+        text:"Qui a réalisé le film Jurassic Park ?",
+        options:["Ridley Scott","Steven Spielberg","Robert Zemeckis"],
+        correct_answer: "Steven Spielberg"
+      },
+
+      {
+        text:"Quel est la durée du film le plus long du monde ?",
+        options:["857 heures","10 heures et 43 minutes","9 heures"],
+        correct_answer: "857 heures"
+      },
+      // Autres questions...
+    ]
+  };
