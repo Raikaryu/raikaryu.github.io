996ae8984ddd7f24bad0160161db7dfb1ce7b025
Raika <pejuvenelle@hotmail.com>
2024-10-30 11:23:32 +0100

    Correction fichier Game et index HTML

--- game.js
+++ game.js
@@ -1,11 +1,12 @@
 import { quiz_cinema } from './questions.js';
 const questions = document.getElementById("questions-container");
-//const options = document.getElementById("options-container");
-const firstQuestion = quiz_cinema.questions[0].text
-questions.innerHTML = firstQuestion;
-firstQuestion.quiz_cinema.forEach(options => {
-    const options = document.createElement('button');
-    options.innerText = quiz_cinema.questions[0].options;
-    __________.classList.add('__________');
-    __________.appendChild(__________);
+const options = document.getElementById("options-container");
+const firstQuestion = quiz_cinema.questions[0]
+questions.innerText = firstQuestion.text;
+firstQuestion.options.forEach(answer => {
+    const choix = document.createElement('button');
+    choix.innerText = answer;
+    choix.classList.add('options-container');
+    options.appendChild(choix);
   });
+ 
